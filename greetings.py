6175730ca5c3be842df58f995d0756e07b1f3346
Marie-Rose <65497176+Marie-rgb@users.noreply.github.com>
2024-02-22 12:08:09 +0300

    python assignment

--- greetings.py
+++
@@ -1,3 +0,0 @@
-print("Hello there!")
-
-#ways of commenting
--- user_input.py
+++ user_input.py
@@ -1,13 +1,8 @@
-#name = input('Enter your name here:')
-#age = input('What is your age?')
-#location = input('Where are you located?')
+name = input('Enter your name here:')
+age = input('What is your age?')
+location = input('Where are you located?')
 
 
-#print("Welcome on board[name] of age[age] from[location )
-#print("Hello ", name,"of age", age, "from the well known city of ",location)
+print("Hello ", name,"of age", age, "from the well known city of ",location)
 
 
-x = {}
-x[2] = 10
-x[1] = [20, 30, 40]
-print(x[1][2])
