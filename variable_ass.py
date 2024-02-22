3e1ed9a9530e1ca2b9b770a05d45fee1e6b4ba4d
Marie-Rose <65497176+Marie-rgb@users.noreply.github.com>
2024-02-22 12:05:45 +0300

    python assignment

---
+++ variable_ass.py
@@ -0,0 +1,23 @@
+import site
+site_name = 'Power learn'
+
+print(site_name)
+
+#assigning new value to site_name
+site_name = 'You'
+
+
+
+
+#converting integer to float
+integer_number = 123
+float_number = 1.23
+
+new_number = integer_number + float_number
+print("Value:",new_number)
+print("Data_type:", type(new_number))
+
+
+
+num = int(input('Enter a number:'))
+print('Data type of num:', type(num))
